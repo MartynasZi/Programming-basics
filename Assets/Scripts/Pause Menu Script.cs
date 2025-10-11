93f26653df450bbd3bc86a4821ac49b8ba719a34
Bethaknee <Martynas19970821@gmail.com>
2025-10-12 00:34:11 +0200

    Basic Menu Implementation
    Added:
    1. Main menu
    2. Options menu
    3. In-Game menu (accessed via the Escape key)

---
+++ Assets/Scripts/Pause Menu Script.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    public string menuScene;
+
+    [Header("Pause Menu Settings")]
+    public GameObject pauseScreen;
+    public GameObject pauseMenu;
+    public GameObject optionsMenu;
+    private bool isPaused = false;
+    public KeyCode pauseKey = KeyCode.Escape;
+
+    public void GoToMainMenu ()
+    {
+        
+        SceneManager.LoadScene(menuScene);
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void Start()
+    {
+        Debug.Log(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+        Cursor.visible = isPaused;
+        Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+    void Update ()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            TogglePauseMenu();
+        }
+    }
+    public void TogglePauseMenu()
+    {
+        if (optionsMenu != null && optionsMenu.activeSelf)
+        {
+            optionsMenu.SetActive(false);
+            if (pauseMenu != null) pauseMenu.SetActive(true);
+            return;
+        }
+        else
+        {
+            isPaused = !isPaused;
+            if (pauseScreen != null) pauseScreen.SetActive(isPaused);
+            Time.timeScale = isPaused ? 0f : 1f;
+            Cursor.visible = isPaused;
+            Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }    
+}
