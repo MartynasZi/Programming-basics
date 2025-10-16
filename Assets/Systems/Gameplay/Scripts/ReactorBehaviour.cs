ba01e29c0ef8a3b80e5e47589a4f9bf1acaebaef
Gustav Eckrodt <g.eckrodt@gmail.com>
2025-10-16 15:38:52 +0200

    Started on Rod Scripting

--- Assets/Systems/Gameplay/Scripts/ReactorBehaviour.cs
+++ Assets/Systems/Gameplay/Scripts/ReactorBehaviour.cs
@@ -9,8 +9,12 @@ public class ReactorBehaviour : MonoBehaviour
     [Header("Control Rod Settings")]
     [SerializeField] private Vector2 _minMaxLaunchForce = new Vector2(8f, 12f);
 
+    public int _RodsInReactor;
+
+
     private void Start()
     {        
+        InitRods();
     }
 
     private void Update()
@@ -27,5 +31,21 @@ public class ReactorBehaviour : MonoBehaviour
 
         controlRodBehaviour.LaunchControlRod(_minMaxLaunchForce.x, _minMaxLaunchForce.y);   //Finally launch the rod with the random values our controller dictates.
     }
+
+    private void InitRods()
+    {
+        //Method called at the beginning when all Rods are still present. We count how many rods are present and assign an ID to each Rod.
+        int RodID = 1;
+
+        foreach (StaticControlRod rod in gameObject.GetComponentsInChildren<StaticControlRod>()) 
+        { 
+            rod._ControlRodID = RodID;
+            rod._InReactor = true;
+
+            _RodsInReactor = RodID;
+            RodID++;
+        }
+    }
+
 
 }
